ff54d5c163a09c466ef7cf977dcd49160b7d1da6
erik G <egomezfax19@gmail.com>
2021-02-04 16:03:18 -0800

    base portfolio page

---
+++ src/components/card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+export default function PortCard() {
+	return (
+		<>
+			<Card>
+				<Card.Img />
+				<Card.Body>
+					<Card.Title></Card.Title>
+					<Card.Text></Card.Text>
+				</Card.Body>
+			</Card>
+		</>
+	);
+}
---
+++ src/pages/portfolioPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CardGroup from "react-bootstrap/CardGroup";
+import PortCard from "../components/card";
+
+export default function Portfolio() {
+	return (
+		<>
+			<CardGroup>
+				<PortCard />
+				<PortCard />
+				<PortCard />
+				<PortCard />
+				<PortCard />
+				<PortCard />
+			</CardGroup>
+		</>
+	);
+}
