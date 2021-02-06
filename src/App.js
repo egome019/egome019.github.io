542f1e58dfd50807e07b48e25282c890ad6c399b
erik G <egomezfax19@gmail.com>
2021-02-06 11:31:49 -0800

    added pages to appjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Header from "./components/header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AboutPage from "./pages/aboutPage";
+import PortfolioPage from "./pages/portfolioPage";
+import ContactPage from "./pages/contactPage";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<AboutPage />
+			<PortfolioPage />
+			<ContactPage />
 		</div>
 	);
 }
