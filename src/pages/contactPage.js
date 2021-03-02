4196e35754fcaead139bd0f5fd345796801fb3bc
erik G <egomezfax19@gmail.com>
2021-03-01 21:50:55 -0800

    worked on contact page, still need to add media queries

--- src/pages/contactPage.js
+++ src/pages/contactPage.js
@@ -2,21 +2,51 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { IconContext } from "react-icons";
+import { FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import "./contactPage.css";
 
 export default function ContactPage() {
 	return (
 		<div id="contact">
-			<Container className="bg-dark mt-3 contactPage">
-				<Row>
-					<h1>Contact</h1>
-				</Row>
-				<Row>
-					<Col sm>sm=true></Col>
-					<Col sm>sm=true></Col>
-					<Col sm>sm=true></Col>
-				</Row>
-			</Container>
+			<IconContext.Provider
+				value={{ style: { fontSize: "100px", color: "rgb(255, 235, 205)" } }}
+			>
+				<Container className="bg-dark mt-3 contactPage">
+					<Row>
+						<h1>Contact</h1>
+					</Row>
+					<Row>
+						<Col sm>
+							<a
+								href="https://www.linkedin.com/in/erik-gomez-61ba22b0/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FaLinkedinIn />
+							</a>
+						</Col>
+						<Col sm>
+							<a
+								href="https://twitter.com/egomez_8_"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FaTwitter />
+							</a>
+						</Col>
+						<Col sm>
+							<a
+								href="https://www.instagram.com/gomeze019/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FaInstagram />
+							</a>
+						</Col>
+					</Row>
+				</Container>
+			</IconContext.Provider>
 		</div>
 	);
 }
