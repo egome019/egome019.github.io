a81efb6c29f4cd827e14ebd635b65d2dec2d5aee
erik G <egomezfax19@gmail.com>
2021-03-07 12:13:49 -0800

    added skills page, not complete

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AboutPage from "./pages/aboutPage";
 import PortfolioPage from "./pages/portfolioPage";
 import ContactPage from "./pages/contactPage";
+import SkillsPage from "./pages/skills";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<AboutPage />
+			<SkillsPage />
 			<PortfolioPage />
 			<ContactPage />
 		</div>
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { IconContext } from "react-icons";
+import "./skillsPage.css";
 
 export default function SkillsPage() {
 	return (
@@ -8,7 +9,10 @@ export default function SkillsPage() {
 			<IconContext.Provider
 				value={{ style: { fontSize: "75px", color: "rgb(255, 235, 205)" } }}
 			>
-				<Container>
+				<Container className="bg-dark mt-3 skillsContainer">
+					<div className="skillsHeader">
+						<h1>Skills</h1>
+					</div>
 					<Row>
 						<Col></Col>
 					</Row>
